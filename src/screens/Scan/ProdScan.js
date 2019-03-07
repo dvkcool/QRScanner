92ad455355ebd874ca66af364a5a5111f8ad75de
Divyanshu Kumar <divyanshukumarg@gmail.com>
2019-03-07 19:32:29 +0530

    working basic qr code scanner

--- src/screens/Scan/ProdScan.js
+++ src/screens/Scan/ProdScan.js
@@ -1,16 +1,34 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
-
-
+import {View, Text, TouchableOpacity} from 'react-native';
+import QRCodeScanner from 'react-native-qrcode-scanner';
+import styles from '../../../styles/Scanner';
 export default class ProdScan extends Component<Props> {
   static navigationOptions = {
     title: 'GST Scanner - Product Scanning',
   };
+  onSuccess(e) {
+    console.log(e);
+
+  }
+
   render() {
     return (
-      <View>
-        <Text> Product scan page</Text>
-      </View>
+      <QRCodeScanner
+        onRead={this.onSuccess.bind(this)}
+        topContent={
+          <Text style={styles.centerText}>
+            Scan QR code to add product to bill
+          </Text>
+        }
+        bottomContent={
+          <TouchableOpacity style={styles.buttonTouchable} onPress={() => this.props.navigation.navigate('Home')}>
+            <Text style={styles.buttonText}>OK. Got it!</Text>
+          </TouchableOpacity>
+        }
+        reactivate = {true}
+        reactivateTimeout = {5}
+      />
     );
   }
+
 }
